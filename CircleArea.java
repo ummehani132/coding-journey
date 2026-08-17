b5ac5dae834b411517476f1d60591ab28fe1bf56
ummehani132 <umme83103@gmail.com>
2026-08-17 16:56:14 +0530

    Add files via upload

---
+++ CircleArea.java
@@ -0,0 +1,10 @@
+package codingjourney;
+
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area of Circle = " + area);
+    }
+}
