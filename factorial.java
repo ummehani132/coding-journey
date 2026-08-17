fc62c0106d1f200d1f801df8c48cc4fe60adc597
ummehani132 <umme83103@gmail.com>
2026-08-17 15:25:40 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package codingjourney;
+
+public class factorial {
+
+	public static void main(String[] args) {
+	     int n = 5;
+	     int factorial = 1;
+	     for (int i=1;i<=n;i++) {
+	    	 factorial = factorial*i;
+	    	 
+	     }
+	     System.out.println("Factorial="+factorial);
+
+	}
+
+}
