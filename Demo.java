ac4f55daab3470aa28e0ab8529b42cdb1b03c6a5
ummehani132 <umme83103@gmail.com>
2026-08-17 19:44:32 +0530

    Added Demo Program

---
+++ Demo.java
@@ -0,0 +1,43 @@
+package codingjourney;
+
+public class Demo {
+
+    // Instance variable
+    int a = 66;
+
+    // Static variable
+    static int cc = 99;
+
+    // Method
+    void m1() {
+        // Local variable
+        int a = 10;
+
+        System.out.println("Local variable a = " + a);
+    }
+
+    // Constructor
+    public Demo() {
+        int b = 10;
+
+        System.out.println("Constructor variable b = " + b);
+    }
+
+    public static void main(String[] args) {
+
+        // Creating an object
+        Demo test = new Demo();
+
+        // Calling the method
+        test.m1();
+
+        // Accessing the instance variable
+        System.out.println("Instance variable a = " + test.a);
+
+        // Accessing the static variable
+        System.out.println("Static variable cc = " + cc);
+
+        // Static variable can also be accessed using the class name
+        System.out.println("Static variable using class name = " + Demo.cc);
+    }
+}
