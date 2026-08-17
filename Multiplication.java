755015c30985f0d34b24389903929e8ba0834c7f
ummehani132 <umme83103@gmail.com>
2026-08-17 17:00:54 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+package codingjourney;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int product = a * b;
+
+        System.out.println("Product = " + product);
+    }
+}
