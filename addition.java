45e6d0b9fc3312bb436c453d2ed6d9703badc40c
ummehani132 <umme83103@gmail.com>
2026-08-17 14:39:54 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,15 @@
+package codingjourney;
+
+public class addition {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		int sum = a + b;
+
+		System.out.println("Sum = " + sum);
+
+	}
+
+}
