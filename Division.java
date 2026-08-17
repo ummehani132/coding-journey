b9b791d7341dd473739ffcf42d1188fce1bc8578
ummehani132 <umme83103@gmail.com>
2026-08-17 17:03:58 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,12 @@
+package codingjourney;
+
+public class Division {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+
+        int result = a / b;
+
+        System.out.println("Division = " + result);
+    }
+}
